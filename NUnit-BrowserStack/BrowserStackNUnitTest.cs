0741b83532ce1a62a99cdbba77ec800ad12217e6
rajnishk952 <rajnish@browserstack.com>
2020-09-11 19:54:52 +0530

    Initial Commit

---
+++ NUnit-BrowserStack/BrowserStackNUnitTest.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+
+
+namespace BrowserStack
+{
+  [TestFixture]
+  public class BrowserStackNUnitTest
+  {
+    protected IWebDriver driver;
+    protected string profile;
+    protected string environment;
+    //private Local browserStackLocal;
+
+    public BrowserStackNUnitTest(string profile, string environment)
+    {
+      this.profile = profile;
+      this.environment = environment;
+    }
+    
+    [SetUp]
+    public void Init()
+    {
+      NameValueCollection caps = ConfigurationManager.GetSection("capabilities/" + profile) as NameValueCollection;
+      NameValueCollection settings = ConfigurationManager.GetSection("environments/" + environment) as NameValueCollection;
+
+      DesiredCapabilities capability = new DesiredCapabilities();
+
+      foreach (string key in caps.AllKeys)
+      {
+        capability.SetCapability(key, caps[key]);
+      }
+
+      foreach (string key in settings.AllKeys)
+      {
+        capability.SetCapability(key, settings[key]);
+      }
+
+      String username = Environment.GetEnvironmentVariable("BROWSERSTACK_USERNAME");
+      if(username == null)
+      {
+        username = ConfigurationManager.AppSettings.Get("user");
+      }
+
+      String accesskey = Environment.GetEnvironmentVariable("BROWSERSTACK_ACCESS_KEY");
+      if (accesskey == null)
+      {
+        accesskey = ConfigurationManager.AppSettings.Get("key");
+      }
+
+      capability.SetCapability("browserstack.user", username);
+      capability.SetCapability("browserstack.key", accesskey);
+      Object local_cap = capability.GetCapability("browserstack.local");
+      if (local_cap != null && local_cap.ToString().Equals("true"))
+       {
+        //browserStackLocal = new Local();
+            List<KeyValuePair<string, string>> bsLocalArgs = new List<KeyValuePair<string, string>>() {
+                    new KeyValuePair<string, string>("key", accesskey)
+            };
+            // BrowserStackLocal.exe file won't work on Mac.
+            //Language Bindings attempts to start the 'exe' file.
+            //For Mac, please start BrowserStackLocal Mac binary separately on command line.
+
+            //browserStackLocal.start(bsLocalArgs);
+       }
+
+      driver = new RemoteWebDriver(new Uri("http://"+ ConfigurationManager.AppSettings.Get("server") +"/wd/hub/"), capability);
+    }
+
+    [TearDown]
+    public void Cleanup()
+    {
+      driver.Quit();
+      //if (browserStackLocal != null)
+      //{
+      //  browserStackLocal.stop();
+      //}
+    }
+  }
+}
